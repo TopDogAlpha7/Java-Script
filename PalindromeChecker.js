1ce99e0d80b3cdaf4cd25670de094986b74bc4f1
Tinodaishe Simbanegavi <tinoronsimbanegavi1234@gmail.com>
2023-02-15 21:00:17 +0200

    final stage

--- PalindromeChecker.js
+++ PalindromeChecker.js
@@ -1,5 +1,15 @@
- function palindrome(str) {
-  return true;
-}
-
-palindrome("eye");
+function palindrome(str) {
+    let regex = /[^A-Za-z0-9]/g
+    let cleansingString = str.replace(regex, "").toLowerCase().split("")
+  
+    let newString = cleansingString.join("")
+    let reverseString = cleansingString.reverse().join("")
+    
+    if (newString === reverseString) {
+      return true
+    } else {
+      return false
+    }
+  }
+  
+  console.log(palindrome("My age is 0, 0 si ega ym."));
